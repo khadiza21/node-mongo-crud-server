8541e4eeb69be3efc8a9577477d7ca098ccb42d4
khadiza21 <bibikhadiza474@gmail.com>
2022-04-30 19:36:53 +0600

    node mongo crud server express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("running my node curd server");
+});
+
+app.listen(port, () => {
+  console.log("curd server is running");
+});
